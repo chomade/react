a0a9e91757b8fb9b0f406dd4c4789c0d9ce07f3c
Bruce0203 <BruceFreedy@gmail.com>
2023-08-07 01:01:54 +0900

    implementation

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -4,6 +4,7 @@ import korlibs.image.font.*
 import korlibs.image.text.*
 import korlibs.io.async.*
 import korlibs.io.file.std.*
+import korlibs.io.lang.*
 import korlibs.korge.*
 import korlibs.korge.input.*
 import korlibs.korge.scene.*
@@ -51,6 +52,7 @@ class MyScene : Scene() {
             val rectText = uiText("")
                 .alignX(root, 0.1, true)
                 .alignY(root, 0.1, true)
+            lateinit var cancellable: Cancellable
             mouse.onDown {
                 println("asdf")
                 when(clickMode) {
@@ -62,13 +64,15 @@ class MyScene : Scene() {
                             startTime = DateTime.now()
                             middleText.text = ""
                             background.color = ColorPalette.GREEN
-                        }
+                        }.also { cancellable = it.cancellable() }
                     }
                     PLAY -> {
                         background.color = ColorPalette.GRAY
                         endTime = DateTime.now()
                         if (endTime < startTime) {
                             middleText.text = "화면이 녹색으로 바뀌면 클릭하십시오. 준비가 되었으면 클릭하십시오"
+                            cancellable.cancel()
+                            clickMode = IDLE
                             return@onDown
                         }
                         rectText.text = "반응 시간: ${endTime - startTime}"
